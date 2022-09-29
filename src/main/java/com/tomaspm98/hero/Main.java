19165a90e7c2bd6669d6b7fc86e62a1f1da580c4
tomaspm98 <up202006845@fe.up.pt>
2022-09-29 17:12:22 +0100

    Application

--- src/main/java/com/tomaspm98/hero/Main.java
+++ src/main/java/com/tomaspm98/hero/Main.java
@@ -1,7 +1,32 @@
 package com.tomaspm98.hero;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            TerminalSize terminalSize=new TerminalSize(40,20);
+            DefaultTerminalFactory terminalFactory=new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal1 =terminalFactory.createTerminal();
+
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+            screen.clear();
+            screen.setCharacter(10,10, TextCharacter.fromCharacter('X')[0]);
+            screen.refresh();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
